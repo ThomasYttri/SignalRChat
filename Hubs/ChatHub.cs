f738fb53dd82f37187f2f75b94f4eaf9a4489ea4
Thomas Valvik Yttri <thoytt04@hotmail.com>
2021-02-17 16:54:08 +0100

    Added project

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs {
+    public class ChatHub : Hub {
+        public async Task SendMessage(string user, string message) {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
